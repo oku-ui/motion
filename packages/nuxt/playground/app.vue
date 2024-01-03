1d5d10447a3b5bc06db3ffdf084ce375e5c24b47
productdevbook <hi@productdevbook.com>
2024-01-03 22:15:41 +0300

    feat: add nuxt module

--- packages/nuxt/playground/app.vue
+++ packages/nuxt/playground/app.vue
@@ -1,8 +1,59 @@
-<script setup>
+<script setup lang="ts">
+import { Motion } from '@oku-ui/motion'
+
+const rotate = ref(10)
 </script>
 
 <template>
-  <div>
-    Nuxt module playground!
+  <Motion
+    :animate="{
+      rotate: `${rotate}deg`,
+      backgroundColor: 'var(--yellow)' }"
+    :transition="{
+      duration: 1,
+      rotate: { duration: 2 },
+    }"
+  />
+
+  <div
+    :style="{
+      marginTop: '50px',
+    }"
+  >
+    <button @click="rotate += 20">
+      Rotate
+    </button>
   </div>
+
+  <Motion
+    :initial="{ opacity: 0, x: -50 }"
+    :in-view="{
+      opacity: 1,
+      x: 0,
+      transition: {
+        delay: 0.5,
+        easing: [0.17, 0.55, 0.55, 1],
+        duration: 0.9,
+      },
+    }"
+  >
+    <p>
+      Lorem Ipsum
+    </p>
+  </Motion>
 </template>
+
+<style>
+body {
+  padding: 100px;
+}
+:root {
+  --yellow: #000
+}
+
+div {
+  width: 100px;
+  height: 100px;
+  border-radius: 10px;
+}
+</style>
--- packages/nuxt/src/module.ts
+++ packages/nuxt/src/module.ts
@@ -1,35 +1,50 @@
-import { addPlugin, createResolver, defineNuxtModule } from '@nuxt/kit'
+import { addComponent, defineNuxtModule } from '@nuxt/kit'
 
-// Module options TypeScript interface definition
 export interface ModuleOptions {
-  apiKey: string
+  autoImportComponents?: boolean
+  prefix?: string
 }
 
-export interface ModuleHooks {
-  'my-module:init': any
-}
+// export interface ModuleHooks {
+//   'my-module:init': any
+// }
 
-export interface ModulePublicRuntimeConfig {
-  NAME: string
-}
+// export interface ModulePublicRuntimeConfig {
+//   NAME: string
+// }
 
-export interface ModuleRuntimeConfig {
-  PRIVATE_NAME: string
-}
+// export interface ModuleRuntimeConfig {
+//   PRIVATE_NAME: string
+// }
+
+const motionComponents = [
+  'Motion',
+  'Presence',
+  'PresenceGroup',
+]
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
-    name: 'my-module',
-    configKey: 'myModule',
+    name: '@oku-ui/motion-nuxt',
+    configKey: 'motion',
   },
-  // Default configuration options of the Nuxt module
   defaults: {
-    apiKey: '',
+    autoImportComponents: true,
   },
-  setup(_options, _nuxt) {
-    const resolver = createResolver(import.meta.url)
+  setup(options, _nuxt) {
+    // const resolver = createResolver(import.meta.url)
+
+    if (options.autoImportComponents) {
+      motionComponents.forEach((component) => {
+        addComponent({
+          name: `${options.prefix}${component}`,
+          export: component,
+          filePath: '@oku-ui/motion',
+        })
+      })
+    }
 
     // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
-    addPlugin(resolver.resolve('./runtime/plugin'))
+    // addPlugin(resolver.resolve('./runtime/plugin'))
   },
 })
