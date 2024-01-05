e97bf335d3cf79f093e535c5bb3026e14cebee3e
productdevbook <hi@productdevbook.com>
2024-01-05 11:06:33 +0300

    feat: add types import and export from @motionone/dom

--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -1,3 +1,5 @@
 export { default as Motion } from './component/Motion.vue'
 export { default as Presence } from './component/Presence.vue'
 export { default as PresenceGroup } from './component/PresenceGroup.vue'
+
+export * from '@motionone/dom'
