46fe30b45708b6ff7e725dff60510eb2348000bf
YeSuX <351490949@qq.com>
2024-03-10 16:25:48 +0800

    feat: add new docs

---
+++ .docs/components/logo.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { Application } from '@splinetool/runtime'
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  const canvas = document.getElementById('logo')
+  const app = new Application(canvas)
+  app.load('https://prod.spline.design/xnZjafuaOrjFtT0C/scene.splinecode')
+})
+</script>
+
+<template>
+  <canvas id="logo" />
+</template>
