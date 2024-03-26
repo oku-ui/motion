2bb9c31c226b9874f0a4d8bd006fbeabd6a06771
YeSuX <351490949@qq.com>
2024-03-26 16:30:04 +0800

    feat: add use-animte demo

---
+++ packages/vue/playground/src/excamples/use-animate.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <div class="box" :style="{ width: '50px', height: '50px', background: 'blue' }" />
+    <p>reverse</p>
+    <button @click="play()">
+      play
+    </button>
+  </div>
+</template>
