315af48485aae5720e041f1cf686f0c46f1001d1
productdevbook <hi@productdevbook.com>
2024-11-18 10:36:03 +0300

    chore: fix build

--- packages/core/src/global.d.ts
+++ packages/core/src/global.d.ts
@@ -5,3 +5,9 @@ type VisualElementStore = typeof visualElementStore
 declare module 'framer-motion/dist/es/render/store.mjs' {
   export const visualElementStore: VisualElementStore
 }
+
+declare module '@oku-ui/motion' {
+  import type { DefineComponent } from 'vue'
+
+  export const Motion: DefineComponent<any, any, any>
+}
--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -1,87 +1,65 @@
-import process from 'node:process'
-
-// import { externalizeDeps } from 'vite-plugin-externalize-deps'
-
-import { execSync } from 'node:child_process'
-import { resolve } from 'node:path'
+import path from 'node:path'
 import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import { globbySync } from 'globby'
-import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  define: {
-    __DEV__: process.env.NODE_ENV !== 'production',
-  },
   plugins: [
-    // externalizeDeps(),
-    vue(),
-    vueJsx(),
+    vue() as any,
+    vueJsx() as any,
     dts({
+      cleanVueFileName: true,
       outDir: 'dist',
-      exclude: ['src/**/__tests__/*', 'src/**/stories/*'],
-      compilerOptions: {
-        composite: false,
-        declaration: true,
-        declarationMap: true,
-      },
-      tsconfigPath: 'tsconfig.app.json',
-      afterBuild: async () => {
-        // pnpm build:plugins
-        execSync('pnpm build:plugins', { stdio: 'inherit', cwd: resolve(__dirname, '../plugins') })
-        // execSync('pnpm lint:fix', { stdio: 'inherit', cwd: resolve(__dirname, '../..') })
-      },
-    }),
-  ],
-  build: {
-    copyPublicDir: false,
-    minify: false,
-    sourcemap: true,
-    lib: {
-      name: 'oku-ui-primitives',
-      formats: ['es'],
-      entry: [
-        ...globbySync('src/**/*.ts', { ignore: [
-          '**/__tests__/**',
-          '**/stories/**',
-          '**/*.stories.ts',
-        ] }),
-        'src/index.ts',
+      exclude: [
+        'src/test/**',
+        '**/stories/**',
+        'src/**/*.stories.vue',
+        '**/.docs/**', // .docs klasörünü hariç tut
+        'src/components/stories/**',
       ],
+      tsconfigPath: 'tsconfig.build.json',
+    }),
+
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      'framer-motion/dist/es/animation/utils/create-visual-element.mjs': path.resolve(__dirname, 'node_modules/framer-motion/dist/es/animation/utils/create-visual-element.mjs'),
+      'framer-motion/dist/es/render/store.mjs': path.resolve(__dirname, 'node_modules/framer-motion/dist/es/render/store.mjs'),
     },
-    target: 'esnext',
+  },
+  build: {
+    outDir: 'dist',
+    lib: {
+      name: 'motion-vue',
+      fileName: (format, name) => {
+        return `${name}.${format === 'es' ? 'js' : 'umd.cjs'}`
+      },
+      entry: {
+        index: path.resolve(__dirname, 'src/index.ts'),
+      },
+    },
     rollupOptions: {
       output: {
-        esModule: true,
-        preserveModules: true,
-        preserveModulesRoot: 'src',
-        entryFileNames: '[name].mjs',
+        globals: {
+          'vue': 'Vue',
+          '@oku-ui/primitives': '@oku-ui/primitives',
+        },
       },
       external: [
         'vue',
-        '@vue/shared',
-        'motion',
         '@oku-ui/primitives',
-        'hey-listen',
-        'framer-motion',
-        '@vueuse/core',
+        '@oku-ui/motion', // Motion modülünü dışla
+        fileURLToPath(
+          new URL(
+            'src/components/stories',
+            import.meta.url,
+          ),
+        ),
       ],
     },
   },
-  resolve: {
-    alias: {
-      '@oku-ui': fileURLToPath(new URL('./src', import.meta.url)),
-      '@/': fileURLToPath(new URL('./src', import.meta.url)),
-      'framer-motion/dist/es/animation/utils/create-visual-element.mjs': resolve(__dirname, 'node_modules/framer-motion/dist/es/animation/utils/create-visual-element.mjs'),
-      'framer-motion/dist/es/render/store.mjs': resolve(__dirname, 'node_modules/framer-motion/dist/es/render/store.mjs'),
-    },
-  },
-  // resolve: {
-  //   alias: {
-  //     '~': fileURLToPath(new URL('./src', import.meta.url)),
-  //   },
-  // },
+
 })
